55d3be7701a2a5f0550e71ef393e5dca495f38b9
redfrog999 <redfrog999@gmail.com>
2025-03-17 13:29:21 +0800

    filogic: add xr30-nand support

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -556,6 +556,26 @@ define Device/cmcc_rax3000m-nand-ubootmod
 endef
 TARGET_DEVICES += cmcc_rax3000m-nand-ubootmod
 
+define Device/cmcc_xr30-nand-ubootmod
+  DEVICE_VENDOR := CMCC
+  DEVICE_MODEL := XR30 NAND version
+  DEVICE_VARIANT := (custom U-Boot layout)
+  DEVICE_DTS := mt7981b-cmcc-xr30-nand-ubootmod
+  DEVICE_DTS_DIR := ../dts
+  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware \
+	kmod-usb3 automount
+  SUPPORTED_DEVICES := cmcc,xr30
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 116736k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += cmcc_xr30-nand-ubootmod
+
 define Device/cmcc_rax3000me
   DEVICE_VENDOR := CMCC
   DEVICE_MODEL := RAX3000Me
