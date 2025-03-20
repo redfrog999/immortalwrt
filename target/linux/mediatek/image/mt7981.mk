e5b9654a0328fc307cffe75e7687827962b859a0
NicolasMe9907 <mooner.nicolas999@proton.me>
2025-03-20 16:55:08 +0800

    Cudy TR3000v1 : Expand to 114M

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -478,7 +478,7 @@ define Device/cudy_tr3000-v1
    UBINIZE_OPTS := -E 5
    BLOCKSIZE := 128k
    PAGESIZE := 2048
-   IMAGE_SIZE := 65536k
+   IMAGE_SIZE := 114688k
    KERNEL_IN_UBI := 1
    IMAGES += factory.bin
    IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
