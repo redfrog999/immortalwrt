94433b80cf995e351a6b25886207a39af1f9573e
NicolasMe9907 <mooner.nicolas999@proton.me>
2025-03-20 21:01:50 +0800

    Update mt7981.mk

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -468,15 +468,13 @@ define Device/cmcc_rax3000m-emmc
 endef
 TARGET_DEVICES += cmcc_rax3000m-emmc
 
-define Device/cmcc_xr30-nand-ubootmod
+define Device/cmcc_xr30-usboffload
   DEVICE_VENDOR := CMCC
-  DEVICE_MODEL := XR30 NAND version
-  DEVICE_VARIANT := (custom U-Boot layout)
-  DEVICE_DTS := mt7981b-cmcc-xr30-nand-ubootmod
-  DEVICE_DTS_DIR := ../dts
-  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7981-firmware mt7981-wo-firmware kmod-usb3 \
-	automount f2fsck mkf2fs
-  SUPPORTED_DEVICES := cmcc,xr30
+  DEVICE_MODEL := XR30 NAND USBOFFLOAD
+  DEVICE_DTS := mt7981-cmcc-xr30-usboffload
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) swconfig luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
+  SUPPORTED_DEVICES := cmcc,xr30-usboffload
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
@@ -486,7 +484,20 @@ define Device/cmcc_xr30-nand-ubootmod
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += cmcc_xr30-nand-ubootmod
+TARGET_DEVICES += cmcc_xr30-usboffload
+
+
+define Device/cmcc_xr3000m-emmc-usboffload
+  DEVICE_VENDOR := CMCC
+  DEVICE_MODEL := XR30 eMMC USBOFFLOAD
+  DEVICE_DTS := mt7981-cmcc-xr30-emmc-usboffload
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := cmcc,xr30-emmc-usboffload
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) swconfig f2fsck losetup mkf2fs kmod-fs-f2fs kmod-mmc \
+        luci-app-ksmbd luci-i18n-ksmbd-zh-cn ksmbd-utils
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += cmcc_xr30-emmc-usboffload
 
 define Device/cudy_tr3000-v1
    DEVICE_VENDOR := Cudy
